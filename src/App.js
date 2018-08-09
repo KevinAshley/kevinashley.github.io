86b8343c6debaeb17802acb007e002ce1b39435d
Kevin Ashley <kevbo1985@gmail.com>
2018-08-08 23:12:46 -0700

    finalizing

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.<br />
-            <Button>Kevin testing</Button>
+      <Button>Kevin testing again</Button>
         </p>
       </div>
     );
