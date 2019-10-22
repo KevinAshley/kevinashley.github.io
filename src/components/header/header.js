1891c2fb66ea35b5d82fe5dca6c56287664d90c0
Kevin Ashley <kevbo1985@gmail.com>
2019-10-21 21:35:08 -0700

    color changes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-        <header className="app-header text-primary bg-dark">
+        <header className="app-header text-light bg-dark">
           <i className="fas fa-compact-disc App-logo"></i>
           <h1 className="app-title text-light">Kevin Ashley</h1>
         </header>
